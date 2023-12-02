59b369f5dd8e78032f9f1764f301c9a854702df5
Pablo Fernandes Sena <pabupablo2000@gmail.com>
2023-12-02 14:04:37 -0300

    commit 02/12
    Adição de javascript na página de fale conosco.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,25 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('form');
     const campos = document.querySelectorAll('.required');
 
     function validarCampos() {
-        let camposVazios = false;
-
-        campos.forEach(campo => {
-            if (campo.value.trim() === "") {
-                camposVazios = true;
-            }
-        });
-
-        if (camposVazios) {
+        if ([...campos].some(campo => campo.value.trim() === "")) {
             alert("Por favor, preencha todos os campos obrigatórios.");
         } else {
             const dados = {
                 nome: document.querySelector('[name="nome"]').value,
                 email: document.querySelector('[name="replyto"]').value,
+                telefone: document.querySelector('[name="telefone"]').value,
                 comentarios: document.querySelector('textarea').value
             };
 
-       
-            alert(`Dados enviados com sucesso:\nNome: ${dados.nome}\nE-mail: ${dados.email}\nComentários: ${dados.comentarios}`);
-
-      
+            alert(`Pedido registrado com sucesso:\nNome: ${dados.nome}\nE-mail: ${dados.email}\nTelefone: ${dados.telefone}\nPedido: ${dados.comentarios}`);
         }
     }
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); 
+        event.preventDefault();
         validarCampos();
     });
 });
